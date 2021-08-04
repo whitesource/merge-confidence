9ca32d4a9a1da539d04b6e04b1545348e73d68ed
L. H. N <82917144+l198881@users.noreply.github.com>
2021-08-04 13:23:16 -0300

    Update and rename beta.json to beta.ts

--- beta.ts
+++ beta.ts
@@ -1,15 +1,15 @@
-{
+
   "prBodyDefinitions": {
     "Age": "[![age](https://badges.renovateapi.com/packages/{{datasource}}/{{replace '/' '%2f' depName}}/{{{toVersion}}}/age-slim)](https://docs.renovatebot.com/merge-confidence/)",
     "Adoption": "[![adoption](https://badges.renovateapi.com/packages/{{datasource}}/{{replace '/' '%2f' depName}}/{{{toVersion}}}/adoption-slim)](https://docs.renovatebot.com/merge-confidence/)",
     "Passing": "[![passing](https://badges.renovateapi.com/packages/{{datasource}}/{{replace '/' '%2f' depName}}/{{{toVersion}}}/compatibility-slim/{{{fromVersion}}})](https://docs.renovatebot.com/merge-confidence/)",
     "Confidence": "[![confidence](https://badges.renovateapi.com/packages/{{datasource}}/{{replace '/' '%2f' depName}}/{{{toVersion}}}/confidence-slim/{{{fromVersion}}})](https://docs.renovatebot.com/merge-confidence/)"
-  },
+  
   "packageRules": [
     {
       "datasources": ["maven", "npm", "pypi"],
       "updateTypes": ["patch", "minor", "major"],
       "prBodyColumns": ["Package", "Change", "Age", "Adoption", "Passing", "Confidence"]
-    }
-  ]
-}
+    
+  
+
